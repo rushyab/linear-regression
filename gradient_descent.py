86f2f5f15cc524d448439895c84166b81c336100
Rushyab <rushyab@gmail.com>
2017-09-08 16:50:46 +0530

    comments added

--- gradient_descent.py
+++ gradient_descent.py
@@ -4,16 +4,23 @@ import matplotlib.pyplot as plt
 N = 10
 D = 3
 X = np.zeros((N, D))
-X[:,0] = 1 # bias term
-X[:5,1] = 1 # assign first 5 values as 1
-X[5:,2] = 1	# assign last 5 values as 1
+X[:,0] = 1   # bias term
+X[:5,1] = 1  # assign first 5 values as 1
+X[5:,2] = 1	 # assign last 5 values as 1
 Y = np.array([0]*5 + [1]*5)
 
 # print X so you know what it looks like
 print("X:", X)
 
-# won't work!
+# won't work! X.T.dot(X) is a singular matrix
 # w = np.linalg.solve(X.T.dot(X), X.T.dot(Y))
+
+# Gradient descent: w = w - learning_rate*(d(cost)/dw)
+# cost function in matrix form: (Y- Yhat)**2 = (Y - Xw).T (Y - Xw)
+# solving further: Y.T Y - Y.T (Xw) - (Xw).T Y + (Xw).T (Xw)
+# diffrentiate cost function w.r.t w:
+# d(cost)/dw = - 2 X.T Y + 2 X.T (Xw) = 2 X.T (Yhat - Y)
+# can drop 2, as it gets absorbed by learning rate
 
 # let's try gradient descent
 costs = [] # keep track of squared error cost
